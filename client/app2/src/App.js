80e637a4b62a3df553d97b7bf7e84e3ffc8acbd3
unknown <muhdqozeem@gmail.com>
2022-10-05 09:44:58 -0400

    created app2

---
+++ client/app2/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
